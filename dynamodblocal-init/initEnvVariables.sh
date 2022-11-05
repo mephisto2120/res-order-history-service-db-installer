8ed48d34d5e050091eb57ddc09b8bf5442904842
mephisto <mephisto2120@gmail.com>
2022-11-05 15:16:24 +0100

    Using docker compose for DynamoDb

--- dynamodblocal-init/initEnvVariables.sh
+++ dynamodblocal-init/initEnvVariables.sh
@@ -1,4 +1,4 @@
 #! /bin/bash -e
-export AWS_DYNAMODB_ENDPOINT_URL=http://test-dynamodblocal-init:8000
+export AWS_DYNAMODB_ENDPOINT_URL=http://localhost:8000
 export AWS_ACCESS_KEY_ID=FOO_ID
 export AWS_SECRET_ACCESS_KEY=FOO_SECRET
